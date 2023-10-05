d1b51bf54d375c74ef316a0da220bf00f732dc60
Retreat-hunter <hunterkorsen@gmail.com>
2023-10-05 14:46:28 -0700

    Done

--- javascript.js
+++ javascript.js
@@ -1,13 +1,46 @@
 // 1.1 Assignment: Where do you put JavaScript?
-
+alert ("javascript is easy");
 // 1.2 Assignment: Variables
-
+var john_lastName="Smith";
+console.log(john_lastName);
+var john_firstName="John";
+console.log(john_firstName);
+var john_gender="uni";
+console.log(john_gender);
+var john_age="25";
+console.log(john_age);
+var john_country="United Kingdom";
+console.log(john_country);
 // 1.3 Assignment: Variable Declaration & Initialization
-
+var ReTreaT_firstLetter="R";
+console.log(ReTreaT_firstLetter);
+var ReTreaT_secondLetter="e";
+console.log(ReTreaT_secondLetter);
+var ReTreaT_thirdLetter="T";
+console.log(ReTreaT_thirdLetter);
+var ReTreaT_forthLetter="r";
+console.log(ReTreaT_forthLetter);
+var ReTreaT_fifthLetter="e";
 // 1.4 Assignment: Complete your statements
-
+x = 2; y = 3;
+z = x + y;
 // 1.5 Assignment: Brackets, Braces and Parentheses
-
+var a = function(){
+    alert('Statement 1');
+   alert('Statement 2');
+}
 // 1.6 Assignment: Data Types
+var H="Hunter";
+console.log("FirstName:",H)
+var J="21";
+console.log("Age", J)
+var D="You like Coffee";
+console.log("Do you like coffee?",D)
 
 // 1.7 Assignment: Practicing JavaScript basics
+var A=4;
+var B=3;
+var C=5;
+
+var D=(B*A/2)
+console.log("area", D);
